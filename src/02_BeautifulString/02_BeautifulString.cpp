7ee84d6cb76d8824024987cadd368852df9761af
Shining Yang <y.s.n@live.com>
2015-08-03 00:19:32 +0800

    Add support for considering more than 3 different chars as B-String

--- src/02_BeautifulString/02_BeautifulString.cpp
+++ src/02_BeautifulString/02_BeautifulString.cpp
@@ -66,7 +66,7 @@ bool BeautifulStringParser::Find(const TCHAR* str, int& offset, int& length)
         if (_FeedChar(*p, p - str)) {
             _TrimHead();
             offset = m_pItems[0].offset;
-            length = m_pItems[0].count * BS_CHAR_NUM;
+            length = m_pItems[0].count * m_nCharNum;
             return true;
         }
     }
@@ -96,21 +96,23 @@ bool BeautifulStringParser::_FeedChar(TCHAR ch, int offset)
                 m_nIndicator = 0;
             }
         } else if (m_pItems[m_nIndicator].data + 1 == ch) {
-            if ((m_nIndicator == 0) || (m_nIndicator == 1)) {
+			if ((m_nIndicator == 0) || m_pItems[m_nIndicator].count == m_pItems[m_nIndicator - 1].count) {
                 m_nIndicator++;
                 m_pItems[m_nIndicator].data = ch;
                 m_pItems[m_nIndicator].count = 1;
                 m_pItems[m_nIndicator].offset = offset;
-            } else { // indicator is 2
-                m_pItems[0].data = m_pItems[1].data;
-                m_pItems[0].count = m_pItems[1].count;
-                m_pItems[0].offset = m_pItems[1].offset;
-                m_pItems[1].data = m_pItems[2].data;
-                m_pItems[1].count = m_pItems[2].count;
-                m_pItems[1].offset = m_pItems[2].offset;
-                m_pItems[2].data = ch;
-                m_pItems[2].count = 1;
-                m_pItems[2].offset = offset;
+            } else {
+				// shift items
+                m_pItems[0].data	= m_pItems[m_nIndicator - 1].data;
+                m_pItems[0].count	= m_pItems[m_nIndicator - 1].count;
+                m_pItems[0].offset	= m_pItems[m_nIndicator - 1].offset;
+                m_pItems[1].data	= m_pItems[m_nIndicator].data;
+                m_pItems[1].count	= m_pItems[m_nIndicator].count;
+                m_pItems[1].offset	= m_pItems[m_nIndicator].offset;
+                m_pItems[2].data	= ch;
+                m_pItems[2].count	= 1;
+                m_pItems[2].offset	= offset;
+				m_nIndicator = 2;
             }
             if (_Check()) {
                 return true;
@@ -129,19 +131,11 @@ bool BeautifulStringParser::_FeedChar(TCHAR ch, int offset)
 
 bool BeautifulStringParser::_Check() const
 {
-    if (m_nIndicator < BS_CHAR_NUM - 1) { // must feed all
+    if (m_nIndicator < m_nCharNum - 1) { // must feed all
         return false;
     }
 
-#if 0 // Redundant verification
-    for (int i = 0; i < BS_CHAR_NUM - 1; i++) {
-        if (m_pItems[i + 1].data - m_pItems[i].data != 1) { // increasing
-            return false;
-        }
-    }
-#endif
-
-    if (m_pItems[BS_CHAR_NUM - 1].count == m_pItems[BS_CHAR_NUM - 2].count) {
+    if (m_pItems[m_nCharNum - 1].count == m_pItems[m_nCharNum - 2].count) {
         return true;
     }
 
@@ -162,22 +156,39 @@ bool FindBeautifulString(const TCHAR* str, int& offset, int& length)
     return parser.Find(str, offset, length);
 }
 
+void OutputResult(const TCHAR* text, bool found, int offset, int length)
+{
+	_tprintf(_T("%s\n"), found ? _T("YES") : _T("NO"));
+
+	if (found) {
+		for (int i = 0; i < length; i++) {
+			_tprintf(_T("%c"), text[offset + i]);
+		}
+		_tprintf(_T("\n"));
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-    TCHAR text[] = _T("aabbbcccddeefg");
+    TCHAR text[] = _T("aabbbcccddeefghi");
 
     int offset = -1;
     int length = -1;
-    bool found = FindBeautifulString(text, offset, length);
-    _tprintf(_T("%s\n"), found ? _T("YES") : _T("NO"));
+	bool found = false;
+	
+	found = FindBeautifulString(text, offset, length);
+	OutputResult(text, found, offset, length);
 
-    if (found) {
-        for (int i = 0; i < length; i++) {
-            _tprintf(_T("%c"), text[offset + i]);
-        }
-        _tprintf(_T("\n"));
-    }
+	//===========================
+	BeautifulStringParser parser(4);
+	found = parser.Find(text, offset, length);
+	OutputResult(text, found, offset, length);
 
-    return 0;
+	//===========================
+	BeautifulStringParser parser2(5);
+	found = parser2.Find(text, offset, length);
+	OutputResult(text, found, offset, length);
+
+	return 0;
 }
 
