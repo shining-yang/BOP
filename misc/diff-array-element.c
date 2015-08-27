16c5213f2d62b25c519e16aabe485d7ecb04f619
Shining <y.s.n@live.com>
2015-08-27 15:44:31 +0800

    Different element within array

---
+++ misc/diff-array-element.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Give an array of integers, every element appears 3 times except for one.
+ * Find that one.
+ */
+int find_except(int a[], int n)
+{
+	int one = 0;
+	int acc = 0;
+
+	for (int i = 0; i < n; i++) {
+		acc |= a[i] & one;
+		one ^= a[i];
+
+		int t = one & acc;
+		one &= ~t;
+		acc &= ~t;
+	}
+
+	return one;
+}
+
+int main(int argc, char** argv)
+{
+	int a[] = { -1, -2, -1, -1 };
+	int ex = find_except(a, sizeof(a) / sizeof(a[0]));
+	printf("Exception: %d\n", ex);
+
+	int b[] = { 1, 1, -2, 3, 5, 1, -2, 3, -2, 3 };
+	int ex2 = find_except(b, sizeof(b) / sizeof(b[0]));
+	printf("Exception: %d\n", ex2);
+	return 0;
+}
