e4ff0ed31f07dc5085e8bf3847133c51bb3277bb
Shining Yang <y.s.n@live.com>
2016-07-30 12:04:27 +0800

    initialization of variable sized array

---
+++ c++11/variable-sized-object.cpp
@@ -0,0 +1,33 @@
+// @file variable-sized-object.cpp
+//
+#include <iostream>
+#include <vector>
+#include <string>
+
+int TaskMain(int argc, char* argv[]) {
+  std::cout << "TaskMain() argc: " << argc << std::endl;
+  for (int i = 0; i < argc; i++) {
+    std::cout << "\t> " << argv[i] << std::endl;
+  }
+  return 0;
+}
+
+void TestVaribleSizedObject(const std::vector<std::string>& vec_strings) {
+  // initialization of variable sized array in C++11 (ubuntu 16.04, g++ 5.4)
+  const char* args[vec_strings.size()] = {};
+  int n;
+  for (n = 0; n < vec_strings.size(); n++) {
+    args[n] = vec_strings[n].c_str();
+  }
+
+  int rc = TaskMain(n, const_cast<char**>(args));
+  (void)rc;
+}
+
+int main()
+{
+  TestVaribleSizedObject({});
+  TestVaribleSizedObject({ "echo", "bash!" });
+  TestVaribleSizedObject({ "sonar", "start", "-D", "/dev/tty-4" });
+  return 0;
+}
