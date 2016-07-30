bc7966ae504ae4fb58ac8130b07e3840de4c0cca
Shining Yang <y.s.n@live.com>
2016-07-30 15:52:39 +0800

    smart-pointer with deleter

--- c++11/smart-ptr.cpp
+++ c++11/smart-ptr.cpp
@@ -16,11 +16,39 @@ int RunCaseConstruct() {
 }
 
 int RunCaseDeleter() {
-  std::shared_ptr<std::string> sp { new std::string("lambda-deleter"),
+  //----------------------------------------------------------------------------
+  std::shared_ptr<std::string> sp1 { new std::string("lambda-deleter"),
     [](std::string* ptr){
-      std::cout << "shared_ptr<> gets destroyed" << std::endl;
+      // std::cout << "shared_ptr<> gets destroyed" << std::endl;
       delete ptr;
-    }};
+    }
+  };
+
+  //----------------------------------------------------------------------------
+  // Creating a shared_ptr for an array is possible but wrong
+  //
+  // If you use new[] to create an array of objects you have to define your
+  // own deleter. You can do that by passing a function, function object,
+  // or lambda, which calls delete[] for the passed ordinary pointer.
+  std::shared_ptr<int> sp2 { new int[5] { 1, 3, 5, 7, 9 },
+    [](int* p) {
+      delete[] p;
+    }
+  };
+
+  //----------------------------------------------------------------------------
+  std::unique_ptr<int[]> up(new int[10]); // OK
+  //std::shared_ptr<int[]> sp(new int[10]); // ERROR: does not compile
+
+  //----------------------------------------------------------------------------
+  // For unique_ptrs, you have to specify a second template argument to
+  // specify your own deleter
+  std::unique_ptr<double, void (*)(double*)> up2 { new double(3.1415),
+    [](double* ptr) {
+      delete ptr;
+    }
+  };
+
   return 0;
 }
 
